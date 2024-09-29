dfa1cc3ad47f3846ae7eedb3b5ad7dfc5a6b3949
user <asdasd@gmail.com>
2024-09-29 23:54:26 +0300

    Nats dsn added

--- src/controller/mainController.go
+++ src/controller/mainController.go
@@ -14,7 +14,7 @@ type MainControllerImpl struct {
 }
 
 func (u MainControllerImpl) Start(b *gotgbot.Bot, ctx *ext.Context) error {
-	_, err := ctx.EffectiveMessage.Reply(b, fmt.Sprintf("<b>Welcome to Farm⭐️ \n\n Start growing asap! Press Farm!</b>"), &gotgbot.SendMessageOpts{
+	_, err := ctx.EffectiveMessage.Reply(b, fmt.Sprintf("<b>Welcome to Farm⭐️\n\nStart growing asap! Press Farm!</b>"), &gotgbot.SendMessageOpts{
 		ParseMode: "html",
 	})
 	if err != nil {
